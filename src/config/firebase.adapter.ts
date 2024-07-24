5ee07e92e3b1bb5b8a8f9fb10dc689e40dffd16c
David <163231244+The2Deivi@users.noreply.github.com>
2024-07-24 14:36:26 -0500

    next commit

---
+++ src/config/firebase.adapter.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKRhf2NJVI121xpD8B7xw7LWgu9Kl341M",
+  authDomain: "a-motors-d4572.firebaseapp.com",
+  projectId: "a-motors-d4572",
+  storageBucket: "a-motors-d4572.appspot.com",
+  messagingSenderId: "1049026556079",
+  appId: "1:1049026556079:web:f2ee7d423b170ae520e45a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
